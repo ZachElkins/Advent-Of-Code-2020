2464f47433f1de535115f927f9f76e5765025183
ZachElkins <zach.elkins@gmail.com>
2020-12-22 16:15:01 -0500

    reorrganized, need to fix overflow still

--- day03/day03.scala
+++ day03/day03.scala
@@ -5,15 +5,15 @@ object Day03 {
         // MARK: General Setup
         val filename = "input.txt"
         val slope: List[String] = Source.fromFile(filename).getLines.toList
- 5
-        val paths: List[(Int, Int)] = List((1, 1), (3, 1), (5, 1),(7, 1), (1, 2))
-        var productOfTrees = 1
         
         // Part 1
         // val treeCount = countTreesHit(slope, 0, 3, 1, 0)
         // println(s"Careful! Going down this slope you'd hit $treeCount trees!")
 
         // Part 2
+        val paths: List[(Int, Int)] = List((1, 1), (3, 1), (5, 1),(7, 1), (1, 2))
+        var productOfTrees = 1
+
         for (path <- paths) {
             val treeCount = countTreesHit(slope, 0, path._1, path._2, 0)
             println(s"Careful! Going right ${path._1}, left ${path._2} you'd hit $treeCount trees!")
