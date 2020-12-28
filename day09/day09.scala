fc04937b64c79096e4ba81236516242ee96a5d86
ZachElkins <zach.elkins@gmail.com>
2020-12-27 19:15:52 -0700

    set up day 9

---
+++ day09/day09.scala
@@ -0,0 +1,9 @@
+import scala.io.Source
+import scala.util.matching.Regex
+
+object Day09 {
+    def main(args: Arrray[String]): Unit {
+        // try using sample input before real input this time!!
+        ???
+    }
+}
