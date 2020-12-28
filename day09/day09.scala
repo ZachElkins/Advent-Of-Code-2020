8e29de8d77705b1c3ce31bcd82236ecc4ddb5790
ZachElkins <zach.elkins@gmail.com>
2020-12-28 14:04:40 -0700

    Finished Day 9 Part 1

--- day09/day09.scala
+++ day09/day09.scala
@@ -2,8 +2,28 @@ import scala.io.Source
 import scala.util.matching.Regex
 
 object Day09 {
-    def main(args: Arrray[String]): Unit {
-        // try using sample input before real input this time!!
-        ???
+    def main(args: Array[String]): Unit = {
+        val filename = "input.txt"
+        val lst: List[Long] = Source.fromFile(filename).getLines.toList.map(_.toLong)
+        
+        val preambleLength = 25
+
+        val invalidNum = findInvalidNumber(lst, preambleLength)
+        println(s"The invalid number is $invalidNum")
+
+    }
+
+    def findInvalidNumber(numbers: List[Long], preambleLength: Int): Long = {
+        val targetNum = numbers(preambleLength)
+        var flag: Boolean = false
+        val slice = numbers.slice(0, preambleLength)
+
+        for (num1 <- slice)
+            for (num2 <- slice)
+                if (num1 != num2 && num1+num2 == targetNum)
+                    flag = true
+                    
+        if (flag) findFirstNotSum(numbers.drop(1), preambleLength)
+        else targetNum
     }
 }
