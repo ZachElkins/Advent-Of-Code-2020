481a54fbc30e0b003c60631d9c52637d92ea6c58
ZachElkins <zach.elkins@gmail.com>
2020-12-28 14:38:26 -0700

    Finished Day 9 Part 2

--- day09/day09.scala
+++ day09/day09.scala
@@ -3,13 +3,20 @@ import scala.util.matching.Regex
 
 object Day09 {
     def main(args: Array[String]): Unit = {
+        // MARK: General Setup
         val filename = "input.txt"
         val lst: List[Long] = Source.fromFile(filename).getLines.toList.map(_.toLong)
         
+        // MARK: Part 1
         val preambleLength = 25
-
         val invalidNum = findInvalidNumber(lst, preambleLength)
         println(s"The invalid number is $invalidNum")
+
+        // MARK: Part 2
+        val numbers = findNumbersAddingToSum(lst, invalidNum, 0, 2).sortWith(_ > _)
+        val sum = numbers.head + numbers.last
+        println(s"$numbers sum to $invalidNum. The highest and lowest sum to $sum")
+        
 
     }
 
@@ -23,7 +30,17 @@ object Day09 {
                 if (num1 != num2 && num1+num2 == targetNum)
                     flag = true
                     
-        if (flag) findFirstNotSum(numbers.drop(1), preambleLength)
+        if (flag) findInvalidNumber(numbers.drop(1), preambleLength)
         else targetNum
+    }
+
+    def findNumbersAddingToSum(numbers: List[Long], target: Long, startIndex: Int, numbersToCheck: Int): List[Long] = {
+        if(startIndex + numbersToCheck >= numbers.size-1) {
+            findNumbersAddingToSum(numbers, target, 0, numbersToCheck+1)
+        } else {
+            val slice = numbers.slice(startIndex, startIndex + numbersToCheck)
+            if (slice.sum == target) slice
+            else findNumbersAddingToSum(numbers, target, startIndex+1, numbersToCheck)
+        }
     }
 }
