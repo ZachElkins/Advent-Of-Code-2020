f38fa6e0cf6f119f72623d382dbd2f7e198d021f
ZachElkins <zach.elkins@gmail.com>
2021-01-01 14:21:10 -0700

    Final commit. Made it to day 12 by new years.

--- day10/day10.scala
+++ day10/day10.scala
@@ -3,25 +3,69 @@ import scala.io.Source
 object Day10 {
     def main(args: Array[String]): Unit = {
         val filename = "input.txt"
-        val lst = Source.fromFile(filename).getLines.toList.map(_.toInt).sortWith((_ < _))
+        // val lst0 = Source.fromFile(filename).getLines.toList.map(_.toInt).sortWith((_ > _))
 
-        for (l<-lst) println(l)
+        val lst0 = List(28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3).map(_.toInt).sortWith(_ < _)
+        // val lst0 = List(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4).map(_.toInt).sortWith(_ < _)
+
+        val lst = List(0) ++ lst0 ++ List(lst0.last+3)
+        println(lst)
+        // for (l<-lst) println(l)
+
         val jDiffs: (Int, Int) = findJoltDifferences(lst)
         val result: Int = jDiffs._1 * jDiffs._2 
         println(s"There is are ${jDiffs._1} 1-jolt differences and ${jDiffs._2} 3-jolt differences. (${jDiffs._1} * ${jDiffs._2} = $result) ")
+
+        val totalArrangments: Long = findAllArrangments(lst)
+        println(totalArrangments)
     }
 
-    def findJoltDifferences(nums: List[Int], acc: (Int, Int) = (1, 1)): (Int, Int) = {
+    def findJoltDifferences(nums: List[Int], acc: (Int, Int) = (0, 0)): (Int, Int) = {
         if (nums.size <= 1) { 
             acc
         } else {
         val jDiff = nums(1) - nums(0)
             jDiff match {
                 case 1 => findJoltDifferences(nums.tail, (acc._1+1, acc._2))
-                case 2 => findJoltDifferences(nums.tail, (acc._1, acc._2))
                 case 3 => findJoltDifferences(nums.tail, (acc._1, acc._2+1))
-                case _ => throw new IllegalArgumentException("Something went wrong...") 
+                case _ => findJoltDifferences(nums.tail, (acc._1, acc._2))
             }
         }
     }
+
+    def findAllArrangments(nums: List[Int]): Long = {
+        var count: Long = 1
+        for (i <- 1 until nums.size-1) {
+            if (nums(i+1) - nums(i-1) <= 3) {
+                val newNums = nums.slice(0, i) ++ nums.slice(i+1, nums.size)
+                count = count + findAllArrangments(newNums)
+            }
+        }
+        count
+    }
+
+    // def findAllArrangments0(nums: List[Int], acc: Long = 0): Long = {
+    //     // println(s"Testing ${acc}")
+    //     if (nums.size <= 1) {//List.empty) {
+    //         acc + 1
+    //     } else {
+    //         val numsToTest = if (nums.size <= 3) nums.size else 3
+    //         var temp: Long = 0
+    //         for (i <- 1 until numsToTest) {
+    //             if(nums(0) - nums(1) <= 3) {
+    //                 findAllArrangments(nums.drop(1), acc+1)
+    //             }
+    //             findAllArrangments(nums.drop(1), acc+1)
+    //         }
+    //         acc+1
+    //     }
+
+    //     // Start from the end
+
+    //     // Look at all that match the 1-3 jolt difference rule
+        
+    //     // Add that to accumulator
+
+    //     // Do the same thing for each other one
+    // }
 }
--- day12/day12.scala
+++ day12/day12.scala
@@ -2,54 +2,98 @@ import scala.io.Source
 
 object Day12 {
     def main(args: Array[String]): Unit = {
+        // MARK: General Setup
         val filename = "input.txt"
         val lst = Source.fromFile(filename).getLines.toList
         // val lst = List(
         //     "F10",
         //     "N3",
         //     "F7",
-        //     "R270",
-        //     "F11",
-        //     "W2",
-        //     "S2"
+        //     "R90",
+        //     "F11"
         // )
 
+        // MARK: Part 1
         val instructions = lst.map(ins => (ins(0).toString, ins.substring(1, ins.size).toInt))
-        val d = readInstructions(instructions)
-        println(d)
+        val d = readInstructionsLiteral(instructions)
         println(s"The ship's Manhattan distance is ${(d._1 + d._2).abs}")
+        
+        // MARK: Part 2
+
+        /*
+            Incorrect attempts
+             - 51039
+             - 48623
+             - 14691
+             - 4855
+        */
+        
+        println("Adjusting for new instructions...")
+        
+        val w = readInstructionsWaypoint(instructions)
+        println(s"The ship's Manhattan distance is ${(w._1 + w._2).abs}")
     }
 
-    def readInstructions(instructions: List[(String, Int)], acc: (Int, Int) = (0, 0), currentDirection: Int = 2): (Int, Int) = {
+    def readInstructionsWaypoint(instructions: List[(String, Int)], acc: (Int, Int) = (0, 0), wayPoint: (Int, Int) = (10, 1)): (Int, Int) = {
         if (instructions == List.empty) {
             acc
         } else {
             instructions(0) match {
                 case (d: String, m: Int) => {
                     d match {
-                        case "N" => readInstructions(instructions.tail, (acc._1, acc._2+m), currentDirection)
-                        case "E" => readInstructions(instructions.tail, (acc._1+m, acc._2), currentDirection)
-                        case "S" => readInstructions(instructions.tail, (acc._1, acc._2-m), currentDirection)
-                        case "W" => readInstructions(instructions.tail, (acc._1-m, acc._2), currentDirection)
+                        case "N" => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2+m))
+                        case "E" => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1+m, wayPoint._2))
+                        case "S" => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2-m))
+                        case "W" => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1-m, wayPoint._2))
                         case "R" => m match {
-                            case 90  => readInstructions(instructions.tail, acc, if(currentDirection+1 > 4) currentDirection+1-4 else currentDirection+1 )
-                            case 180 => readInstructions(instructions.tail, acc, if(currentDirection+2 > 4) currentDirection+2-4 else currentDirection+2 )
-                            case 270 => readInstructions(instructions.tail, acc, if(currentDirection+3 > 4) currentDirection+3-4 else currentDirection+3 )
+                            case 90  => if (signsMatch(wayPoint)) readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2 * -1)) else readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2))
+                            case 180 => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2 * -1))
+                            case 270 => if (signsMatch(wayPoint)) readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2)) else readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2 * -1))
                         }
                         case "L" => m match {
-                            case 90  => readInstructions(instructions.tail, acc, if(currentDirection-1 < 1) 4+(currentDirection-1) else currentDirection-1 )
-                            case 180 => readInstructions(instructions.tail, acc, if(currentDirection-2 < 1) 4+(currentDirection-2) else currentDirection-2 )
-                            case 270 => readInstructions(instructions.tail, acc, if(currentDirection-3 < 1) 4+(currentDirection-3) else currentDirection-3 )
+                            case 90  => if (signsMatch(wayPoint)) readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2)) else readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2 * -1))
+                            case 180 => readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2 * -1))
+                            case 270 => if (signsMatch(wayPoint)) readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1, wayPoint._2 * -1)) else readInstructionsWaypoint(instructions.tail, acc, (wayPoint._1 * -1, wayPoint._2))
+                        }
+                        case "F" => readInstructionsWaypoint(instructions.tail, (acc._1+(m*wayPoint._1), acc._2+(m*wayPoint._2)), wayPoint)
+                    }
+                }
+            }
+        }
+    }
+
+    def readInstructionsLiteral(instructions: List[(String, Int)], acc: (Int, Int) = (0, 0), currentDirection: Int = 2): (Int, Int) = {
+        if (instructions == List.empty) {
+            acc
+        } else {
+            instructions(0) match {
+                case (d: String, m: Int) => {
+                    d match {
+                        case "N" => readInstructionsLiteral(instructions.tail, (acc._1, acc._2+m), currentDirection)
+                        case "E" => readInstructionsLiteral(instructions.tail, (acc._1+m, acc._2), currentDirection)
+                        case "S" => readInstructionsLiteral(instructions.tail, (acc._1, acc._2-m), currentDirection)
+                        case "W" => readInstructionsLiteral(instructions.tail, (acc._1-m, acc._2), currentDirection)
+                        case "R" => m match {
+                            case 90  => readInstructionsLiteral(instructions.tail, acc, if(currentDirection+1 > 4) currentDirection+1-4 else currentDirection+1)
+                            case 180 => readInstructionsLiteral(instructions.tail, acc, if(currentDirection+2 > 4) currentDirection+2-4 else currentDirection+2)
+                            case 270 => readInstructionsLiteral(instructions.tail, acc, if(currentDirection+3 > 4) currentDirection+3-4 else currentDirection+3)
+                        }
+                        case "L" => m match {
+                            case 90  => readInstructionsLiteral(instructions.tail, acc, if(currentDirection-1 < 1) 4+(currentDirection-1) else currentDirection-1)
+                            case 180 => readInstructionsLiteral(instructions.tail, acc, if(currentDirection-2 < 1) 4+(currentDirection-2) else currentDirection-2)
+                            case 270 => readInstructionsLiteral(instructions.tail, acc, if(currentDirection-3 < 1) 4+(currentDirection-3) else currentDirection-3)
                         }
                         case "F" => currentDirection match {
-                            case 1 => readInstructions(instructions.tail, (acc._1, acc._2+m), currentDirection)
-                            case 2 => readInstructions(instructions.tail, (acc._1+m, acc._2), currentDirection)
-                            case 3 => readInstructions(instructions.tail, (acc._1, acc._2-m), currentDirection)
-                            case 4 => readInstructions(instructions.tail, (acc._1-m, acc._2), currentDirection)
+                            case 1 => readInstructionsLiteral(instructions.tail, (acc._1, acc._2+m), currentDirection)
+                            case 2 => readInstructionsLiteral(instructions.tail, (acc._1+m, acc._2), currentDirection)
+                            case 3 => readInstructionsLiteral(instructions.tail, (acc._1, acc._2-m), currentDirection)
+                            case 4 => readInstructionsLiteral(instructions.tail, (acc._1-m, acc._2), currentDirection)
                         }
                     }
                 }
             }       
         }
     }
+
+    def signsMatch(nums: (Int, Int)): Boolean = { (nums._1 > 0 && nums._2 > 0) || (nums._1 < 0 && nums._1 < 0) }
 }
